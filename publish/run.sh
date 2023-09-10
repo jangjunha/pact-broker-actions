7dc76fc1c1bf2a83ace1ccdc340ce9bd8e0302bf
jangjunha <hi@jangjunha.me>
2023-09-10 22:32:37 +0900

    pact-broker publish action

---
+++ publish/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+docker run --rm \
+	-w ${PWD} \
+	-v ${PWD}:${PWD} \
+	-e PACT_BROKER_BASE_URL=$PACT_BROKER_URL \
+	-e PACT_BROKER_USERNAME=$PACT_BROKER_USERNAME \
+	-e PACT_BROKER_PASSWORD=$PACT_BROKER_PASSWORD \
+	pactfoundation/pact-cli \
+	publish \
+	$target \
+	--consumer-app-version $version \
+	--branch $GITHUB_REF_NAME
